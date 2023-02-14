7c7a6d3ab7883d09956c25f0b0e9732b3614a941
amelone3 <amelone3@gatech.edu>
2023-02-15 12:22:55 +1300

    Updates

---
+++ precache-manifest.207452c8c25cc0afe5f9de9471936cf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "62ac95af81cde88b689a",
+    "url": "/fitfinder/static/css/main.dc197a4a.chunk.css"
+  },
+  {
+    "revision": "62ac95af81cde88b689a",
+    "url": "/fitfinder/static/js/main.62ac95af.chunk.js"
+  },
+  {
+    "revision": "56881c25d975ec026bc2",
+    "url": "/fitfinder/static/js/1.56881c25.chunk.js"
+  },
+  {
+    "revision": "7fd1ac592670b4f7efae",
+    "url": "/fitfinder/static/js/2.7fd1ac59.chunk.js"
+  },
+  {
+    "revision": "4e6c36bfb341c6b46d4f",
+    "url": "/fitfinder/static/js/runtime~main.4e6c36bf.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/fitfinder/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6a53fb629fb6fef95e6c5208158c9df0",
+    "url": "/fitfinder/index.html"
+  }
+];
